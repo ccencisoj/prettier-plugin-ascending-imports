dceae891319faaf63c75f2b190845aa9ea7294cc
ascife <bizapro123@gmail.com>
2023-02-23 19:02:07 -0500

    init

--- app/options.ts
+++ app/options.ts
@@ -86,7 +86,7 @@ export const options: {
 		since: '1.15.0',
 		category: 'Global',
 		type: 'choice',
-		default: SORTING_ORDER.DESCENDING,
+		default: SORTING_ORDER.ASCENDING,
 		description: 'Which sorting order to use',
 		choices: [
 			{
